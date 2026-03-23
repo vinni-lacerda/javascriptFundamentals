95423de0d7a3dffee263670a13398384d5c0bea8
vinni-lacerda <vini.lacerda@hotmail.com>
2026-03-23 16:32:03 -0300

    feat: implementing some count functions

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -11,3 +11,37 @@ const getVowelCount = (sentence) => {
 }
 let vowelCount = getVowelCount("Hello, World!");
 console.log(`Vowel Count: ${vowelCount}`);
+
+const getConsonantCount = (sentence) => {
+  let vowel = "aeiou";
+  let count = 0;
+  let cleanSentence = sentence.trim().toLowerCase().replace(/[,.!]/g, "")
+  
+  for(let char of cleanSentence){
+    if(!vowel.includes(char) && char != " "){
+      count++;
+    }
+  }
+  return count;
+};
+
+const consonantCounter = getConsonantCount("Hello, World!");
+console.log(`Consonant Count: ${consonantCounter}`);
+
+const getPunctuationCount = (sentence) => {
+  const cleanSentence = sentence.replace(/[^.,!?']/g, "");
+  let count = 0;
+  for(let char of cleanSentence){
+    count++;
+  };
+  return count;
+};
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+const getWordCount = (sentence) => {
+  let cleanSentence = sentence.trim();
+  return cleanSentence === "" ? 0 : cleanSentence.split(/\s+/).length;
+};
+const wordCount = getWordCount("I love freeCodeCamp");
+console.log(`Words Count: ${wordCount}`);
